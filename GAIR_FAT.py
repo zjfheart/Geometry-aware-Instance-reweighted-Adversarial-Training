5fb6be2a414279779193957fe0ccc1d33133970d
ZFancy <32093003+ZFancy@users.noreply.github.com>
2020-11-10 10:11:13 +0800

    Modify the data load path

--- GAIR_FAT.py
+++ GAIR_FAT.py
@@ -222,19 +222,19 @@ transform_test = transforms.Compose([
 ])
 
 if args.dataset == "cifar10":
-    trainset = torchvision.datasets.CIFAR10(root='~/dataset/cifar-10', train=True, download=True, transform=transform_train)
+    trainset = torchvision.datasets.CIFAR10(root='./data/cifar-10', train=True, download=True, transform=transform_train)
     train_loader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
-    testset = torchvision.datasets.CIFAR10(root='~/dataset/cifar-10', train=False, download=True, transform=transform_test)
+    testset = torchvision.datasets.CIFAR10(root='./data/cifar-10', train=False, download=True, transform=transform_test)
     test_loader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False, num_workers=2)
 if args.dataset == "svhn":
-    trainset = torchvision.datasets.SVHN(root='~/dataset/SVHN', split='train', download=True, transform=transform_train)
+    trainset = torchvision.datasets.SVHN(root='./data/SVHN', split='train', download=True, transform=transform_train)
     train_loader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
-    testset = torchvision.datasets.SVHN(root='~/dataset/SVHN', split='test', download=True, transform=transform_test)
+    testset = torchvision.datasets.SVHN(root='./data/SVHN', split='test', download=True, transform=transform_test)
     test_loader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False, num_workers=2)
 if args.dataset == "mnist":
-    trainset = torchvision.datasets.MNIST(root='~/dataset/MNIST', train=True, download=True, transform=transforms.ToTensor())
+    trainset = torchvision.datasets.MNIST(root='./data/MNIST', train=True, download=True, transform=transforms.ToTensor())
     train_loader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=1,pin_memory=True)
-    testset = torchvision.datasets.MNIST(root='~/dataset/MNIST', train=False, download=True, transform=transforms.ToTensor())
+    testset = torchvision.datasets.MNIST(root='./data/MNIST', train=False, download=True, transform=transforms.ToTensor())
     test_loader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False, num_workers=1,pin_memory=True)
 
 # Resume 
