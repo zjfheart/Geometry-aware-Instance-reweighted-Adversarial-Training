72d8c57e3f2748ae0588cf2624c836ef5bfe87bd
ZFancy <32093003+ZFancy@users.noreply.github.com>
2020-11-15 12:52:38 +0800

    Revise store path

--- GAIR_RST/run_evaluation.sh
+++ GAIR_RST/run_evaluation.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES='0' python3 attack_evaluation.py --model_path='./GAIR_RST/checkpoint-epoch200.pt' --output_suffix='results' &
+CUDA_VISIBLE_DEVICES='0' python3 attack_evaluation.py --model_path='./GAIR_RST_check/checkpoint-epoch200.pt' --output_suffix='GAIR_RST_PGD_plus_results' &
