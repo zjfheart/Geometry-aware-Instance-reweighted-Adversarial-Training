6c6661228d314cbc473354db5bce13d0fd7c7605
ZFancy <32093003+ZFancy@users.noreply.github.com>
2020-11-16 10:08:33 +0800

    Update run_eval.sh

--- GAIR_RST/autoattack/examples/run_eval.sh
+++ GAIR_RST/autoattack/examples/run_eval.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES='0' python3 eval.py --individual --model='../../GAIR_RST/GAIR_RST_results/checkpoint-epoch200.pt' --log_path='./GAIR_RST_AA_results.txt' --save_dir='./GAIR_RST_AA_results' &
+CUDA_VISIBLE_DEVICES='0' python3 eval.py --model='../../GAIR_RST/GAIR_RST_results/checkpoint-epoch200.pt' --log_path='./GAIR_RST_AA_results.txt' --save_dir='./GAIR_RST_AA_results' &
